490552a418f50af73a56a3c7baa0fb6fd063c1bf
Nathan <ngage@smu.edu>
2022-11-18 17:35:00 -0600

    [add] modeling & ROC

--- project.r
+++ project.r
@@ -34,8 +34,7 @@ columnnames <- c(
 train <-
   read.csv("adult.data",
            header = FALSE,
-           na.strings = c("?", " ?", "NA"),
-  )
+           na.strings = c("?", " ?", "NA"),)
 test <-
   read.csv(
     "adult.test",
@@ -51,6 +50,15 @@ names(test) <- columnnames
 # converting char columns to factors
 train <- as.data.frame(unclass(train), stringsAsFactors = T)
 test <- as.data.frame(unclass(test), stringsAsFactors = T)
+
+# needed because test is missing one country to create factors from
+allcountries <-
+  unique(union(train$native.country, test$native.country))
+test$native.country <-
+  factor(test$native.country, levels = allcountries)
+
+str(train)
+str(test)
 
 # VERIFY DATA QUALITY
 
@@ -74,11 +82,11 @@ summary(test$age)
 summary(train$capital.gain)
 summary(test$capital.gain)
 
-nrow(train[train$capital.gain == 99999,])
-nrow(test[test$capital.gain == 99999,])
+nrow(train[train$capital.gain == 99999, ])
+nrow(test[test$capital.gain == 99999, ])
 
-train <- train[!(train$capital.gain == 99999),]
-test <- test[!(test$capital.gain == 99999),]
+train <- train[!(train$capital.gain == 99999), ]
+test <- test[!(test$capital.gain == 99999), ]
 
 # the values are now far more reasonable
 summary(train$capital.gain)
@@ -93,8 +101,8 @@ summary(train$hours.per.week)
 summary(test$hours.per.week)
 
 # drop education.num (same as our education factor)
-train <- train[,!names(train) %in% c("education.num")]
-test <- test[,!names(test) %in% c("education.num")]
+train <- train[, !names(train) %in% c("education.num")]
+test <- test[, !names(test) %in% c("education.num")]
 
 # GIVE SIMPLE AND APPROPRIATE STATISTICS
 
@@ -109,7 +117,8 @@ ggplot(train, aes(x = factor(income))) +
   geom_bar(color = "black", fill = "white") +
   labs(title = "counts of income type",
        subtitle = "across training set",
-       x = "income level",)
+       x = "income level",
+  )
 
 # distribution of age
 ggplot(train, aes(x = age)) +
@@ -117,7 +126,8 @@ ggplot(train, aes(x = age)) +
   geom_density(alpha = .2) +
   labs(title = "distribution of ages",
        subtitle = "across training set",
-       x = "age",)
+       x = "age",
+  )
 
 # let's take a look at the different types of jobs:
 ggplot(train, aes(x = factor(occupation))) +
@@ -138,10 +148,11 @@ ggplot(train, aes(x = factor(occupation))) +
 ggplot(train, aes(x = age)) +
   geom_histogram(aes(y = ..density..), color = "black", fill = "white") +
   geom_density(alpha = .2) +
-  facet_grid( ~ income) +
+  facet_grid(~ income) +
   labs(title = "distribution of ages by income level",
        subtitle = "across training set",
-       x = "age",)
+       x = "age",
+  )
 
 ggplot(train, aes(
   x = age,
@@ -158,7 +169,6 @@ ggplot(train, aes(
   )
 
 # for our own sake, does education level affect income?
-levels(train$education)
 ggplot(train, aes(
   x = factor(
     education,
@@ -201,7 +211,7 @@ ggplot(train, aes(
 # and finally we are interested in seeing the relationship between hours per week and income
 ggplot(train, aes(x = factor(income), fill = factor(income))) +
   geom_bar(aes(y = (..count.. / sum(..count..))), color = "black") +
-  facet_grid( ~ sex) +
+  facet_grid(~ sex) +
   labs(
     title = "income levels by gender",
     subtitle = "across training set",
@@ -210,4 +220,85 @@ ggplot(train, aes(x = factor(income), fill = factor(income))) +
     fill = "income level"
   )
 
+# CLASSIFICATION
 
+# model 1: LOGISTIC REGRESSION
+logmodel <- glm(income ~ ., data = train, family = "binomial")
+logmodel.preds <- predict(logmodel, test, type = "response")
+logmodel.confusion <-
+  table(test$income, ifelse(logmodel.preds < 0.5, 0, 1))
+logmodel.accuracy <-
+  (logmodel.confusion[1] + logmodel.confusion[4]) / sum(logmodel.confusion)
+logmodel.confusion
+logmodel.accuracy
+
+# model 2: RANDOM FOREST
+library(randomForest)
+rfmodel <- randomForest(income ~ ., data = train)
+rfmodel.preds <- predict(rfmodel, test)
+rfmodel.confusion <- table(test$income, rfmodel.preds)
+rfmodel.accuracy <-
+  (rfmodel.confusion[1] + rfmodel.confusion[4]) / sum(rfmodel.confusion)
+rfmodel.confusion
+rfmodel.accuracy
+
+# model 3: SVM
+library('e1071')
+svmmodel <-
+  svm(
+    income ~ .,
+    data = train,
+    type = "C-classification",
+    kernel = "linear",
+    scale = T
+  )
+svmmodel.preds <- predict(svmmodel, test)
+svmmodel.confusion <- table(test$income, svmmodel.preds)
+svmmodel.accuracy <-
+  (svmmodel.confusion[1] + svmmodel.confusion[4]) / sum(svmmodel.confusion)
+svmmodel.confusion
+svmmodel.accuracy
+
+svmmodel2 <-
+  svm(
+    income ~ .,
+    data = train,
+    type = "C-classification",
+    kernel = "polynomial",
+    scale = T
+  )
+svmmodel2.preds <- predict(svmmodel2, test)
+svmmodel2.confusion <- table(test$income, svmmodel2.preds)
+svmmodel2.accuracy <-
+  (svmmodel2.confusion[1] + svmmodel2.confusion[4]) / sum(svmmodel2.confusion)
+svmmodel2.confusion
+svmmodel2.accuracy
+
+svmmodel3 <-
+  svm(
+    income ~ .,
+    data = train,
+    type = "C-classification",
+    kernel = "sigmoid",
+    scale = T
+  )
+svmmodel3.preds <- predict(svmmodel3, test)
+svmmodel3.confusion <- table(test$income, svmmodel3.preds)
+svmmodel3.accuracy <-
+  (svmmodel3.confusion[1] + svmmodel3.confusion[4]) / sum(svmmodel3.confusion)
+svmmodel3.confusion
+svmmodel3.accuracy
+
+# final model is logistic regression model with 84.67855% accuracy
+# honorable mentions: SVM w/ linear kernel & SVM with sigmoid kernel
+
+# creative work: ROC analysis
+library(ROCR)
+pred <- prediction(logmodel.preds, test$income)
+roc <- performance(pred, "tpr", "fpr")
+plot(roc, colorize = T, lwd = 2)
+abline(a = 0, b = 1)
+
+# this is a good ROC, with a solid balance between FPR and TPR. 
+# given that we have imbalanced classes, this means we are acheiving
+# good performance despite class imbalance.
